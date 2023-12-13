ff057bdf1a7762aaefbcf7e62063f9c072274af3
Roberta <robicleta@hotmail.com>
2023-12-13 19:06:32 -0300

    desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
-﻿using DesafioPOO.Models;
+﻿namespace DesafioPOO.Models
+{
+    public class Iphone : Smartphone
+    {
+       
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+        public Iphone(string numero) : base(numero)
+        {
+      Console.WriteLine($"Instalando {nomeApp} na Nokia...");
+        }
+            
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+             Console.WriteLine($"Instalando {nomeApp} no iPhone...");
+        }
+    }
+}
